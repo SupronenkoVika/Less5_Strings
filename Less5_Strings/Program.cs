080a48e3a94646027d5c1e8dff50f02a283327d2
Viktoryia Supronenko <vika.supronenko@icloud.com>
2022-10-24 16:02:21 +0300

    Add project files.

---
+++ Less5_Strings/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Less5_Strings;
+
+string line = File.ReadAllText("D:\\TMS .Net\\Less5_Strings\\Less5_Strings\\string_file.txt");
+Console.WriteLine(line);
+
+actWithStrings actWithStr = new actWithStrings();
+
+Console.WriteLine("Choose the action.");
+Console.WriteLine("'j' - Max amount of numbers in word.");
+Console.WriteLine("'l' - Longest word.");
+Console.WriteLine("'r' - Replace numbers with words.");
+Console.WriteLine("'i' - Only interrogative and negative sentences.");
+Console.WriteLine("'c' - Sentences without commas.");
+Console.WriteLine("'b' - Words with the same letter at the beginning and in the end.");
+
+char act = Convert.ToChar(Console.ReadLine());
+
+switch (act)
+{
+    case 'j':
+        actWithStr.MaxAmountOfNums(line);
+        break;
+
+    case 'l':
+        actWithStr.LongestWord(line);
+        break;
+
+    case 'r':
+        actWithStr.ReplaceNumbersWithWords(line);
+        break;
+
+    case 'i':
+        actWithStr.InterrogativeAndNegativeSentences(line);
+        break;
+
+    case 'c':
+        actWithStr.SentencesWithoutCommas(line);
+        break;
+
+    case 'b':
+        actWithStr.TheSameBeginningAndEnd(line);
+        break;
+}
---
+++ Less5_Strings/actWithStrings.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+namespace Less5_Strings
+{
+    internal class actWithStrings
+    {
+        #region max amount
+        public void MaxAmountOfNums(string str)
+        {
+            string[] textsplit = str.Split('\n', ' ', '.');
+            int Max = 0;
+            int Index = 0;
+            for (int slovo = 0; slovo < textsplit.Length; slovo++)
+            {
+                int currmax = 0;
+                for (int simvol = 0; simvol < textsplit[slovo].Length; simvol++)
+                {
+                    if (char.IsNumber(textsplit[slovo][simvol])) // Где i это слово k смвол 
+                    {
+                        currmax++;
+                    }
+                }
+                if (currmax > Max)
+                {
+                    Max = currmax;
+                    Index = slovo;
+                }
+            }
+            Console.WriteLine("Наибольше цифр в слове " + textsplit[Index]);
+        }
+        #endregion
+
+        #region longest word
+        public void LongestWord(string str)
+        {
+
+            string[] chars = str.Split('\n', ' ', '.', ',');
+            string maxLength = "";
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (maxLength.Length < chars[i].Length)
+                {
+                    maxLength = chars[i];
+                }
+            }
+
+            int count = 0;
+            foreach (string x in chars)
+            {
+                if (x == maxLength)
+                    count++;
+            }
+
+            Console.WriteLine("Longest word: " + maxLength);
+            Console.WriteLine("Repetition count: " + count);
+        }
+        #endregion
+
+        #region replace numbers with words
+        public void ReplaceNumbersWithWords(string str)
+        {
+            var text = str;
+            var numbers = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+                if (char.IsDigit(text[i]))
+                    sb.Append(numbers[text[i] - '0']);
+                else
+                    sb.Append(text[i]);
+
+            text = sb.ToString();
+            Console.WriteLine(text);
+        }
+        #endregion
+
+        #region interrogative and negative sentences
+        public void InterrogativeAndNegativeSentences(string str)
+        {
+
+            string text = str;
+            string[] textsplit = text.Split('.');
+            foreach (string st in textsplit)
+            {
+                if (st.Contains('?') == true || st.Contains('!') == true)
+                {
+                    string trimmed = st.Trim();
+                    Console.WriteLine(trimmed.Substring(0, trimmed.IndexOf('?') + 1));
+                    Console.WriteLine(trimmed.Substring(0, trimmed.IndexOf('!') + 1));
+                }
+            }
+        }
+        #endregion   
+
+        #region without commas
+        public void SentencesWithoutCommas(string str)
+        {
+            string text = str;
+            string[] sentences = text.Split(new[] { '.', '!', '?' });
+            foreach (string st in sentences)
+                if (st.Contains(","))
+                    continue;
+                else Console.WriteLine(st);
+        }
+        #endregion
+
+        #region the same beginning and end
+        public void TheSameBeginningAndEnd(string str)
+        {
+            Console.WriteLine("Words with the same letter at the beginning and in the end: ");
+            var words = str.Split(' ', ',', '!', '?', '.', '-', '\n');
+            foreach (var word in words)
+            {
+                if (word.Length > 1 && word[0] == word[word.Length - 1])
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+        #endregion
+    }
+}
